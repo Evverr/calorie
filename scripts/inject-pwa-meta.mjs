73cf673d8120171de378201f1588e041b2cd9495
EvEr <leto525@gmail.com>
2026-08-26 16:32:32 +0200

    Extend iOS web app beneath status bar

--- scripts/inject-pwa-meta.mjs
+++ scripts/inject-pwa-meta.mjs
@@ -5,7 +5,7 @@ const pwaHead = `
     <meta name="theme-color" content="#F9F1FF" />
     <meta name="mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
     <meta name="apple-mobile-web-app-title" content="Калории" />
     <link rel="manifest" href="/manifest.webmanifest" />
     <link rel="apple-touch-icon" href="/app-icon.png" />`;
